3408e01ca589c8c002d88096a9d3066b039bccc0
whbbit1999 <luckyboy_dong@sina.com>
2026-01-21 15:13:34 +0800

    feat: add nuxt-seo module and some plugin

---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex items-center justify-center h-screen text-4xl font-bold bg-black text-white">
+    Hi there! <b>(•̀ᴗ•́)و ̑̑</b> Welcome to Nuxt3 Base.
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,27 @@
 import tailwindcss from '@tailwindcss/vite'
+import { pascal } from 'radash'
+
+import packageJSON from './package.json'
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
+  app: {
+    head: {
+      meta: [
+        { name: 'author', content: [packageJSON.author].filter(Boolean).join(', ') },
+        { name: 'color-scheme', content: 'light dark' },
+        { 'name': 'generator', 'content': `${pascal(packageJSON.name)} ${packageJSON.version}`, 'data-github-repo': packageJSON.homepage },
+        { name: 'mobile-web-app-capable', content: 'yes' },
+      ],
+
+      templateParams: {
+        separator: '|',
+      },
+      titleTemplate: `%s %separator ${packageJSON.name}`,
+    },
+  },
   modules: [
     '@nuxt/eslint',
     '@vueuse/nuxt',
@@ -12,6 +31,7 @@ export default defineNuxtConfig({
     '@pinia/nuxt',
     'pinia-plugin-persistedstate/nuxt',
     '@nuxtjs/color-mode',
+    '@nuxtjs/seo',
   ],
 
   css: ['~/assets/css/tailwind.css'],
@@ -26,6 +46,32 @@ export default defineNuxtConfig({
     plugins: [
       tailwindcss(),
     ],
+  },
+
+  components: [
+    '~/components',
+    { path: '~/pages', pattern: '**/components/**/*.vue', pathPrefix: true },
+  ],
+
+  pages: {
+    pattern: ['**/*.vue', '!**/components/**', '!**/data/**', '!**/validators/**'],
+  },
+
+  site: {
+    name: packageJSON.name,
+    url: 'https://github.com/Whbbit1999/nuxt-base',
+  },
+
+  routeRules: {
+    '/': { prerender: true },
+
+    // admin/* use spa
+    '/admin/**': { ssr: false },
+  },
+
+  experimental: {
+    extractAsyncDataHandlers: true,
+    typescriptPlugin: true,
   },
 
   eslint: {
